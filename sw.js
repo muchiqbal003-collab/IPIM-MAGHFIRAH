f9aaa29168d40ef808f82612446506561758c611
muchiqbal003-collab <muchiqbal003@gmail.com>
2026-07-27 20:09:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -68,7 +68,7 @@ self.addEventListener('notificationclick', function(event) {
 // CACHE (sama seperti sebelumnya)
 // =============================================
 
-const CACHE_NAME = 'ipim-v11';
+const CACHE_NAME = 'ipim-v01';
 
 const FILES_TO_CACHE = [
   '/IPIM-MAGHFIRAH/',
